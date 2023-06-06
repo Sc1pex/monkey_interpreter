8cdddb6d8d4011537f79b522812b35eabe5bf2a3
Sc1pex <scipex.dev@gmail.com>
2023-06-06 21:29:58 +0300

    Create lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 #![feature(let_chains)]
 #![allow(dead_code)]
 
-mod ast;
-mod evaluator;
-mod lexer;
-mod object;
-mod parser;
-mod repl;
-mod token;
+use interpreter::repl;
 
 fn main() {
     println!("Monkeylang interpreter");
